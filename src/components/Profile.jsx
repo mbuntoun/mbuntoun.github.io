89a87b456b7c0386b6ba4cc0e5ef33121fed9b79
mbuntoun <m.buntoun@aupp.edu.kh>
2026-07-20 21:56:41 +0700

    add name and add responsive

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,6 +2,8 @@ const Profile = () => (
     <section className="profile">
         <div className="pic"></div>
         <div className="summary">
+            <h2>Monyrath BUNTOUN</h2>
+            <br />
             <p>
                 I am an Instructor at American University of Phnom Penh. 
                 My main interests lies in <strong>Natural Language Processing, particularly in low-resource languages, and leveraging open weight models for Computing Education. </strong>
@@ -11,7 +13,6 @@ const Profile = () => (
             <p>
                 As a women in tech, I am very passionate in making computing education accessible to women and other underrepresented groups.
                 In addition, I am committed to developing digital talents in Cambodia through teaching, mentoring, and public engagement.
-                I have also actively represent women in tech by becoming a technical judge in various competitions, and giving public talks.
             </p>           
         </div>
     </section>
