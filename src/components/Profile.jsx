4f2096f7b151edc58d477d9e5890be45c439e040
mbuntoun <m.buntoun@aupp.edu.kh>
2026-07-26 11:31:24 +0700

    add links in profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,5 @@
-import image from "../assets/profile-pic.jpeg" 
+import image from "../assets/profile-pic.jpeg"
+import { Link } from 'react-router-dom'
 
 const Profile = () => (
     <section className="profile">
@@ -13,14 +14,15 @@ const Profile = () => (
             <h2>Monyrath BUNTOUN</h2>
             <br />
             <p>
-                I am an Instructor at American University of Phnom Penh. 
+                I am an <mark><a href="https://www.aupp.edu.kh/faculty/monyrath-buntoun/" target="_blank">Instructor at American University of Phnom Penh</a></mark>. 
                 My main interests lies in <strong>Natural Language Processing, particularly in low-resource languages, and leveraging open weight models for Computing Education. </strong>
                 Before moving to academia, I also have experiences in Front-end Web Development.
             </p>
             <br />
             <p>
                 As a women in tech, I am very passionate in making computing education accessible to women and other underrepresented groups.
-                In addition, I am committed to developing digital talents in Cambodia through teaching, mentoring, and public engagement.
+                In addition, I am committed to developing digital talents in Cambodia through <mark><Link to="/teaching">teaching</Link></mark> ,  
+                <mark><Link to="/community-services">mentoring, and public engagement</Link></mark>.
             </p>           
         </div>
     </section>
